29ee3dc31cf608f05aed2112a531df3d29153e39
Estefany92 <ashley.huertas@udla.edu.ec>
2025-04-29 07:37:59 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Progreso1_EstefaniHuertas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
